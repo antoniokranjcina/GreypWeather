80d1cb658147661da91405cd949d5f04f2fabf4d
antoniokranjcina <54414989+antoniokranjcina@users.noreply.github.com>
2021-06-18 12:14:24 +0200

    GW-1: Add HomeFragment.kt and basic UI

---
+++ app/src/main/java/com/greyp/weather/ui/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.greyp.weather.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.greyp.weather.R
+import com.greyp.weather.databinding.ActivityMainBinding
+import com.greyp.weather.utils.viewBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setNavHost()
+        setToolbar()
+    }
+
+    private fun setNavHost() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navigation_host) as NavHostFragment
+        navController = navHostFragment.findNavController()
+    }
+
+    private fun setToolbar() {
+        (this as AppCompatActivity).setSupportActionBar(binding.toolbar)
+    }
+}
